cdadaf232c80d5047ea41bce1973cb5c97123b45
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-02-23 01:24:32 -0500

    Create Notes.js
    coded notes from class

---
+++ 1-Module/3-week/3-day/Notes.js
@@ -0,0 +1,114 @@
+
+
+// let obj = {
+
+//     ryan: 'instructor',
+//     anthony: 'assistant',
+//     appAcademy: 'bootcamp'
+// }
+
+// let person = 'anthony'
+
+// console.log(obj)
+// console.log(obj.anthony = '');
+// console.log(obj)
+
+// let obj = {
+//     boolean: true,
+//     one: 1,
+//     someArr: ['hello'],
+//     name: 'anthony',
+//     subObj: { name: 'ryan' },
+//     slice: (word) => word.slice(1)
+// }
+
+// let obj = {
+//     a: 1,
+//     p: 2,
+//     l: 1,
+//     e: 1,
+// }
+
+// for (let key in obj) {
+//     console.log(key);
+//     console.log(obj[key])
+//     let value = obj[key];
+//     console.log(value)
+// }
+
+
+// let res = {};
+// let arr = ['red', 'blue'];
+
+// for (let i = 0; i < arr.length; i++) {
+//     let word = arr[i];
+//     for (let j = 0; j < word.length; j++) {
+//         let letter = word[j];
+//         if (res[letter] !== undefined) { //the key value exists
+//             res[letter]++;
+//         } else { //key-value pair doesnt already exist
+//             res[letter] = 1
+//         }
+//     }
+// }
+
+// console.log(res)
+
+// let test = [0, 1, 2, 3];
+
+// for (let el of test) { //for of loops are for array
+//     // let el = test[i];
+//     console.log(el)
+// }
+
+
+
+// console.log(obj.someArr[0]) //['hello']
+// console.log(obj.slice('ryan'))
+
+
+
+let spiderman = {
+    realName: 'Peter Parker',
+    superName: 'Spider-Man',
+    powers: ['web-slinging', 'wall climbing', 'super strength'],
+    numberOfFriends: 2,
+    mentor: {
+        realName: 'Tony Stark',
+        superName: 'Iron Man',
+        powers: ['smart', 'rich'],
+        numberOfFriends: 0,
+    }
+}
+
+// console.table(spiderman)
+
+// let obj = {
+//     a: 1,
+//     p: 2,
+//     l: 1,
+//     e: 1,
+// }
+
+
+// let keysArray = Object.keys(obj) //returns an array of all the keys
+// let valuesArray = Object.values(obj) //returns an array of all the keys
+// let entries = Object.entries(obj)
+// console.log(entries[0][1])
+
+// [ [ 'a', 1 ], [ 'p', 2 ], [ 'l', 1 ], [ 'e', 1 ] ]
+
+
+// let arr = ['a', 'b', 'c', 'd']
+
+
+// let arr = {
+// 0 : 'a',
+// 1: 'b',
+// 2: 'c',
+// 3: 'd'
+
+// }
+
+
+// console.log(arr[2])
