f189d360f63cfca1838fd9b00e4c4affa92259ae
yinyang117 <slcryan2010@yahoo.com>
2023-04-03 22:52:16 -0600

    EOD stuff

--- 2-Module/2-week/1-day/scratch.js
+++ 2-Module/2-week/1-day/scratch.js
@@ -1,22 +1,85 @@
-// class Test {
-//   testFunc() {
-//     debugger;
+
+let strictFunc = () =>{
+  "use strict"
+  for (let i = 0; i < 10; i++) {
+    
+  }
+}
+
+let strictFunc2 = () =>{
+  for (i = 0; i < 10; i++) {
+    
+  }
+}
+
+strictFunc()
+strictFunc2()
+
+// class Cat {
+//   purr() {
+//     console.log("meow");
+//   }
+
+//   purrMore() {
 //     console.log(this)
-//     console.log(`this is equal to test1: ${this === test1}`);
+//     this.purr();
+//   }
+// }
+// let cat = new Cat();
+
+// global.setTimeout(cat.purrMore, 1000);  // 5 sec later: TypeError: this.purr
+//                                         // is not a function
+
+
+// class Dog {
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   changeName() {
+//     this.name = "Layla";
 //   }
 // }
 
-// const test1 = new Test();
-// Calling the method on the instance
-test1.testFunc(); // true
-// Calling the method outside the class
-const test1Func = test1.testFunc; 
-test1Func(); // false
-// Calling the method as a callback
-setTimeout(test1.testFunc, 1000); // false
+// let dog = new Dog("Bowser");
 
-// Using this in Function Declaration Syntax
-function someFunc() {
-  console.log(this);
+// // Note this is **not invoked** - you are assigning the function itself
+// let change = dog.changeName;
+// console.log(change()); // TypeError, due to not recognizing a valid context
+
+// // Your dog still has the same name!
+// console.log(dog); // { name: 'Bowser', changeName: [Function: changeName] }
+
+
+
+// // class Cat {
+// //   purr() {
+// //     console.log("meow");
+// //   }
+
+// //   purrMore() {
+// //     this.purr();
+// //   }
+
+// //   whatIsThis() {
+// //     console.log(this)
+// //   }
+// // }
+// // let cat = new Cat();
+
+// let thisFunc = cat.whatIsThis;
+
+// // let frankenstein = {
+// //   frankFunc: thisFunc,
+// // }
+
+// // frankenstein.frankFunc()
+
+
+
+
+function testMe() {
+  console.log(this); //
 }
-someFunc(); // Global Object
+
+testMe(); 
