354607aee4d6fb04adfb57f682865297a9c02524
yinyang117 <slcryan2010@yahoo.com>
2023-04-11 21:36:09 -0600

    final notes

--- 2-Module/3-week/2-day/scratch.js
+++ 2-Module/3-week/2-day/scratch.js
@@ -1,52 +1,80 @@
-function constantLoop(n) {
-    let total = 0;
-    for (let i = 1; i <= 100; i++) {
-        total += i
-    }
-    return total;
-}
+// let arr = [];
+// for (let n = 0 ; n <= 1000000000000 ; n++) {
+//   arr.push(n);
+// }
 
-function forLoop(n) {
-    let total = 0;
-    for (let i = 1; i <= n; i++) {
-        total += i;
-    }
-    return total;
-}
-
-
-function nestedLoop(n) {
-    let total = 0;
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j <= n; j++) {
-            total += i
-        }
-    }
-    return total;
-}
-
-function doubleNestedLoop(n) {
-    let total = 0;
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j <= n; j++) {
-            for (let k = 1; k <= n; k++)
-                total += i
-        }
-    }
-    return total;
-}
+// class NameError extends Error {
+// 	constructor(name = null, ...params) {
+// 	  super(...params);
+  
+// 	  if (Error.captureStackTrace) {
+// 		Error.captureStackTrace(this, NameError);
+// 	  }
+  
+// 	  this.name = 'NameError';
+// 	  this.message = this.message || `There was a problem with the name '${name}'`;
+// 	}
+//   }
+  
+//   try {
+// 	const name = 5;
+// 	if (typeof name !== 'string') {
+// 	  throw new NameError('Name cannot be a string');
+// 	}
+//   } catch(e) {
+// 	console.log(e);
+//   }
 
 
-let increment = 100;
-for (let n = increment; n <= increment * 100; n += increment) {
-    let startTime = Date.now();
-    constantLoop(n)
-    forLoop(n);
-    nestedLoop(n);
-    doubleNestedLoop(n);
-    let endTime = Date.now();
-    console.log("".concat(endTime - startTime));
-}
+// function constantLoop(n) {
+//     let total = 0;
+//     for (let i = 1; i <= 100; i++) {
+//         total += i
+//     }
+//     return total;
+// }
+
+// function forLoop(n) {
+//     let total = 0;
+//     for (let i = 1; i <= n; i++) {
+//         total += i;
+//     }
+//     return total;
+// }
+
+
+// function nestedLoop(n) {
+//     let total = 0;
+//     for (let i = 1; i <= n; i++) {
+//         for (let j = 1; j <= n; j++) {
+//             total += i
+//         }
+//     }
+//     return total;
+// }
+
+// function doubleNestedLoop(n) {
+//     let total = 0;
+//     for (let i = 1; i <= n; i++) {
+//         for (let j = 1; j <= n; j++) {
+//             for (let k = 1; k <= n; k++)
+//                 total += i
+//         }
+//     }
+//     return total;
+// }
+
+
+// let increment = 100;
+// for (let n = increment; n <= increment * 100; n += increment) {
+//     let startTime = Date.now();
+//     constantLoop(n)
+//     forLoop(n);
+//     nestedLoop(n);
+//     doubleNestedLoop(n);
+//     let endTime = Date.now();
+//     console.log("".concat(endTime - startTime));
+// }
 
 // let pushTimer = (arr, ...thingsToPush) => {
 // 	arr.push(...thingsToPush)
@@ -61,6 +89,7 @@ for (let n = increment; n <= increment * 100; n += increment) {
 // console.time("timer1")
 // console.log(pushTimer([0], ...nums));
 // console.timeEnd("timer1")
+
 // console.log(`Runtime: ${endTime - startTime}ms`);
 
 
@@ -97,7 +126,30 @@ for (let n = increment; n <= increment * 100; n += increment) {
 // console.log(catPair(150000))
 
 
+// function getNumPairsList(n) {
+//     let pairs = [];
 
+//     for (let i = 0 ; i < n ; i++) {
+//         for (let j = 0 ; j < n ; j++) {
+//             pairs.push([i, j]);
+//         }
+//     }
+
+//     return pairs;
+// }
+
+// console.log(getNumPairsList(1000))
+
+// function addNums(arr) {
+// 	let newArr = []
+// 	console.log("hi")
+//     for (let i = 0 ; i < arr.length ; i++) {
+//         newArr.push(arr[i] + 1);
+//     }
+//     return newArr;
+// }
+
+// console.log(addNums([1,2,3,4,5]))
 
 // function addManyNums(n) {
 
@@ -119,14 +171,18 @@ for (let n = increment; n <= increment * 100; n += increment) {
 // 	let total = 0;
   
 // 	for (let i = 0 ; i < num1 ; i++) {
-// 	  total += 1;
+// 	  total += 1;    
 // 	}
-  
+
 // 	for (let i = 0 ; i < num2 ; i++) {
-// 	  total += 1;
+// 		total += 1;
 // 	}
-  
+
 // 	return total;
 //   }
 
-
+//   startTime = Date.now();
+//   addTwoNums(12345678900, 12345678900);
+//   endTime = Date.now();
+  
+//   console.log(`Runtime: ${endTime - startTime}ms`); 
